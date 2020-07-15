ab3b812cad0db17e348f4137ec2064b36380a398
NeurogrammerT <bbstarnique@aol.com>
2020-07-15 12:09:05 -0400

    minor

--- flamehazesociety-ui/src/components/HomepageComponent/HomepageComponent.tsx
+++ flamehazesociety-ui/src/components/HomepageComponent/HomepageComponent.tsx
@@ -1,12 +1,12 @@
 import React from 'react';// you have to import React from react
 
 //you need a capital letter
-export function FirstComponent(props:any){
+export function HomepageComponent(props:any){
     //you have to return jsx
     return (
         //there must be one root level tag in the jsx
         <div>
-            <h3>This is Our First Component</h3>
+            <h3>Welcome to the Central Hub for the Flame Haze Society!</h3>
         </div>
     );//without the paraenthese the interperator tries to put a semicolon after every line of jsx
 }
--- flamehazesociety-ui/src/components/NavBarComponent/NavBarComponent.tsx
+++ flamehazesociety-ui/src/components/NavBarComponent/NavBarComponent.tsx
@@ -45,62 +45,61 @@ export const NavBarComponent: FunctionComponent<any> = (props) => {
 
     let menuItems = []
 
+    menuItems.push(<MenuItem key={'home'} onClick={handleClose}><Link to='/home'>Home</Link></MenuItem>)
     menuItems.push(<MenuItem key={'login'} onClick={handleClose}><Link to='/login'>Login</Link></MenuItem>)
     menuItems.push(<MenuItem key={'register'} onClick={handleClose}><Link to='/register'>Register</Link></MenuItem>)
 
-    if(props.user){
+    if (props.user) {
         menuItems.push(<MenuItem key={'clicker'} onClick={handleClose}><Link to='/clicker'>Clicker</Link></MenuItem>,
-        <MenuItem key={'first'} onClick={handleClose}><Link to='/first'>First</Link></MenuItem>,
-        <MenuItem key={'title'} onClick={handleClose}><Link to='/title'>Title</Link></MenuItem>,
             <MenuItem key={'profile'} onClick={handleClose}><Link to={`/profile/${(props.user) ? props.user.userId : '0'}`}>My Profile</Link></MenuItem>,
             <MenuItem key={'edit'} onClick={handleClose}><Link to={`/edit/${props.user.userId}`}>Edit Profile</Link></MenuItem>)
     }
-    if(props.user && props.user.role.role === 'Admin'){
+    if (props.user && props.user.role.role === 'Admin') {
         menuItems.push(<MenuItem key={'users'} onClick={handleClose}><Link to='/users'>All Users</Link></MenuItem>,)
     }
     return (
-        (props.user)?
-        <nav>
-            <AppBar position="static">
-                <Toolbar className={classes.bar}>
-                    <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}>
-                        {menuItems}
-                    </Menu>
-                    <Typography variant="h4" className={classes.title}>
-                        Flame Haze Society
+        (props.user) ?
+            <nav>
+                <AppBar position="static">
+                    <Toolbar className={classes.bar}>
+                        <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}>
+                            {menuItems}
+                        </Menu>
+                        <Typography variant="h4" className={classes.title}>
+                            Flame Haze Society
                 </Typography>
-                    <ProfilePictureComponent/>
-                </Toolbar>
-            </AppBar>
+                        <ProfilePictureComponent />
+                    </Toolbar>
+                </AppBar>
             </nav>
             :
             <nav>
-            <AppBar position="static">
-                <Toolbar className={classes.bar}>
-                    <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Menu id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}>
-                        {menuItems}
-                    </Menu>
-                    <Typography variant="h4" className={classes.title}>
-                        Flame Haze Society
+                <AppBar position="static">
+                    <Toolbar className={classes.bar}>
+                        <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}>
+                            {menuItems}
+                        </Menu>
+                        <Typography variant="h4" className={classes.title}>
+                            Flame Haze Society
                 </Typography>
-                    <Button><Link color="inherit" to='/login'>Login</Link></Button>
-                    <Button><Link color="inherit" to='/register'>Register</Link></Button>
-                </Toolbar>
-            </AppBar>
+                        <Button><Link color="inherit" to='/login'>Login</Link></Button>
+                        <Button><Link color="inherit" to='/register'>Register</Link></Button>
+                    </Toolbar>
+                </AppBar>
             </nav>
 
     )
