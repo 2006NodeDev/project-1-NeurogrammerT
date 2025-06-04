62ef992c9ade9c6118b5a0145d2d024883e0a82e
Tanique Adams <bbstarnique@gmail.com>
2025-06-04 13:20:02 -0400

    Update App test to check for Flame Haze Society

--- flamehazesociety-ui/src/App.test.tsx
+++ flamehazesociety-ui/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Flame Haze Society text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Flame Haze Society/i);
   expect(linkElement).toBeInTheDocument();
 });
